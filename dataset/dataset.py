0dba31eb19f4a047b50106a9426877c3416d52d2
dodler <avn8068@gmail.com>
2018-07-21 22:40:21 +0300

    dataset refactored

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,188 +1,83 @@
 import os
-import cv2
+
 import os.path as osp
 import torch
-from PIL import Image
 from sklearn.model_selection import train_test_split
-
+from abc import abstractmethod
 from reader.image_reader import PillowReader
 
 
-class DefaultClassProvider(object):
-    def __init__(self, path):
-        self.objs = []
-        self.lbls = []
-        for i, cls in enumerate(os.listdir(path)):
-            t = osp.join(path, cls, os.listdir(cls))
-            t = [v for v in t if v.endswith('.jpg')]
-            self.objs.extend(t)
-            self.lbls.extend([i] * len(t))
-
-    def __getitem__(self, item):
-        return self.objs[item], self.lbls[item]
-
-    def __len__(self):
-        return len(self.lbls)
-
-
 # todo add testing
-class FolderCachingDataset(object):
-    '''
-    assumes that there is a following folder structure
-    path
-        class1
-            img1
-            ...
-            imgn
-        class2
-            img1
-            ...
-            imgn
-        ...
-        classn
-            img1
-            ...
-            imgn
+class GenericXYDataset(object):
 
-    works in 2 modes:
-    train, validation
-    also performs automatic train-val split
-    uses Pillow for image reading
-    caches raw images
-    '''
-
-    def __len__(self):
-        if self.mode == 'train':
-            return len(self.train_labels)
-        else:
-            return len(self.val_labels)
-
-    def setmode(self, mode):
-        self.mode = mode
-
-    def __init__(self, train_transform, val_transform, path_class_provider,
-                 reader=PillowReader()):
+    def __init__(self, x_train_transform, x_val_transform,
+                 y_train_transform, y_val_transform,
+                 xy_paths_provider,
+                 x_reader=PillowReader(), y_reader=PillowReader()):
         super().__init__()
 
-        if train_transform is None or val_transform is None:
+        if x_train_transform is None or x_val_transform is None:
             raise AttributeError('Broken transform')
 
         self.mode = 'train'
-        self.reader = reader
-        images = []
-        labels = []
+        self.reader = x_reader
+        X = []
+        y = []
 
-        for path, cls in path_class_provider:
-            images.append(path)
-            labels.append(cls)
+        for x_path, y_path in xy_paths_provider:
+            X.append(x_path)
+            y.append(y_path)
 
-        self.train, self.val, self.train_labels, self.val_labels = train_test_split(images, labels)
+        self.train_x, self.val_x, self.train_y, self.val_y = train_test_split(X, y)
 
-        self.train_transform = train_transform
-        self.val_transform = val_transform
-        self.cache = {}
+        self.x_train_transform = x_train_transform
+        self.x_val_transform = x_val_transform
+        self.y_train_transform = y_train_transform
+        self.y_val_transform = y_val_transform
+        self.x_cache = {}
+        self.y_cache = {}
 
-    def get_img(self, index):
-        k = str(index) + self.mode
-        if k in self.cache.keys():
-            return self.cache[k]
-        else:
-            if self.mode == 'train':
-                img = self.reader(self.train[index]).convert('RGB')
-            else:
-                img = self.reader(self.val[index]).convert('RGB')
-
-        self.cache[k] = img
-        return img
-
-    def __getitem__(self, index):
-
-        img = None
-        while img is None:
-            try:
-                img = self.get_img(index)
-            except BaseException as e:
-                index += 1
-                print('failed', index, 'exception', e)
-
+    def __len__(self):
         if self.mode == 'train':
-            label = self.train_labels[index]
-            img = self.train_transform(img)
+            return len(self.train_y)
         else:
-            label = self.val_labels[index]
-            img = self.val_transform(img)
-
-        return img, torch.FloatTensor([label])
-
-
-class HierarchicalDataset(object):
-    def __init__(self, path, train_transform, test_transform):
-        from sklearn.model_selection import train_test_split
-
-        self.baseclasses = {}
-        self.subclasses = {}
-        self.files = []
-        self.labels = []
-        idx = 0
-        for base_cl in os.listdir(path):
-            for sc in os.listdir(osp.join(path, base_cl)):
-                for img in os.listdir(osp.join(path, base_cl, sc)):
-                    self.files.append(osp.join(path, base_cl, sc, img))
-                    self.labels.append(idx)
-
-                self.baseclasses[idx] = base_cl
-                self.subclasses[idx] = sc
-                idx += 1
-
-        print(self.baseclasses, self.subclasses)
-
-        self.train, self.test, self.train_l, self.test_l = train_test_split(self.files, self.labels)
-        self.mode = 'train'
-
-        self.cache = {}
-
-        self.train_transform = train_transform
-        self.test_transform = test_transform
+            return len(self.val_y)
 
     def setmode(self, mode):
         self.mode = mode
 
-    def __len__(self):
+    @abstractmethod
+    def read_x(self, index):
+        pass
+
+    @abstractmethod
+    def read_y(self, index):
+        pass
+
+    def select_transform(self):
         if self.mode == 'train':
-            return len(self.train_l)
+            return self.x_train_transform, self.y_train_transform
         else:
-            return len(self.test_l)
+            return self.x_val_transform, self.y_val_transform
 
-    def get_img(self, index):
-        k = str(index) + self.mode
-        if k in self.cache.keys():
-            return self.cache[k]
-        else:
-            if self.mode == 'train':
-                img = Image.open(self.train[index]).convert('RGB')
-            else:
-                img = Image.open(self.test[index]).convert('RGB')
+    def fetch_cache(self, index):
+        key = self.mode + str(index)
+        return self.x_cache[key], self.y_cache[key]
 
-        result = img.copy()
-        img.close()
-        self.cache[k] = result
-        return result
+    def put_cache(self, index, X, y):
+        key = self.mode + str(index)
+        self.x_cache[key] = X
+        self.y_cache[key] = y
 
     def __getitem__(self, index):
-
-        img = self.get_img(index)
-
-        if self.mode == 'train':
-            label = self.train_l[index]
-            img = self.train_transform(img)
+        if self.in_cache(index):
+            X,y = self.fetch_cache(index)
         else:
-            label = self.test_l[index]
-            img = self.test_transform(img)
+            X = self.read_x(index)
+            y = self.read_y(index)
+            self.put_cache(index, X,y)
 
-        return img, torch.LongTensor([label])
+        x_transform, y_transform = self.select_transform()
 
-    def base_classes(self):
-        return self.baseclasses
+        return x_transform(X), y_transform(y)
 
-    def sub_classes(self):
-        return self.subclasses
