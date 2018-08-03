edc71ee869a554939597d8c0ba9448402f3abc02
dodler <avn8068@gmail.com>
2018-08-03 15:53:56 +0300

    added random single channel gamma

--- generic_utils/segmentation/dualcolor.py
+++ generic_utils/segmentation/dualcolor.py
@@ -78,8 +78,8 @@ class SingleChannelGamma(object):
     def __init__(self, value):
         self.value = value
 
-    def __call__(self, gamma, arr):
-        return (np.power(arr, 1.0 / gamma) * 255).astype(np.uint8)
+    def __call__(self, img, gamma):
+        return (np.power(img, 1.0 / gamma) * 255).astype(np.uint8)
 
 
 class Gamma(object):
