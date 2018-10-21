1407943b65892a1192f689c51e62104ae1d885ef
dodler <avn8068@gmail.com>
2018-10-21 11:09:42 +0300

    minor things from salt

--- training/training.py
+++ training/training.py
@@ -2,8 +2,7 @@ import time
 
 import numpy as np
 import torch
-from torch.autograd import Variable
-from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.optim.lr_scheduler import CosineAnnealingLR
 
 from generic_utils.output_watchers import ClassificationWatcher
 from generic_utils.utils import AverageMeter
@@ -17,22 +16,31 @@ TRAIN_LOSS_OUT = 'train loss'
 
 class Trainer(object):
 
-    def __init__(self, criterion, metric, optimizer, model_name, device=0):
+    def __init__(self, criterion, metric, optimizer, model_name, base_checkpoint_name=None, device=0):
         '''
 
         :param watcher_env: environment for visdom
         :param criterion - loss function
         '''
+        if base_checkpoint_name is None:
+            self.base_checkpoint_name = model_name
+        else:
+            self.base_checkpoint_name = base_checkpoint_name
+
         self.metric = metric
         self.criterion = criterion
         self.watcher = VisdomValueWatcher(model_name)
         self.output_watcher = ClassificationWatcher(self.watcher)
         self.optimizer = optimizer
-        self.scheduler = ReduceLROnPlateau(optimizer, 'min')
+        self.scheduler = CosineAnnealingLR(optimizer, eta_min=1e-5, T_max=1e6)
         self.best_loss = np.inf
         self.model_name = model_name
         self.device = device
         self.epoch_num = 0
+        self.epoch_val_losses = []
+        self.epoch_val_metrics = []
+        self.epoch_train_losses = []
+        self.epoch_train_metrics = []
 
     def set_output_watcher(self, output_watcher):
         self.output_watcher = output_watcher
@@ -40,12 +48,15 @@ class Trainer(object):
     def get_watcher(self):
         return self.watcher
 
-    def save_checkpoint(self, state, name):
+    @staticmethod
+    def save_checkpoint(state, name):
         print('saving state at', name)
         torch.save(state, name)
 
     def get_checkpoint_name(self, loss):
-        return self.model_name + '_loss_' + str(loss.detach().cpu().numpy()) + '.pth.tar'
+        print(loss)
+        # return self.base_checkpoint_name + '_loss_' + str(loss) + '.pth.tar'
+        return self.base_checkpoint_name + '_best.pth.tar'
 
     def is_best(self, avg_loss):
         best = avg_loss < self.best_loss
@@ -54,52 +65,59 @@ class Trainer(object):
 
         return best
 
+    def update_val_epoch_stat(self, loss, metric):
+        self.epoch_val_losses.append(loss)
+        self.epoch_val_metrics.append(metric)
+
     def validate(self, val_loader, model):
         batch_time = AverageMeter()
         losses = AverageMeter()
-        acc = AverageMeter()
+        metrics = AverageMeter()
 
-        # switch to evaluate mode
         model.eval()
 
         end = time.time()
         for batch_idx, (input, target) in enumerate(val_loader):
             with torch.no_grad():
-                input_var = Variable(input.to(self.device))
-                target_var = Variable(target.to(self.device))
+                input_var = input.to(self.device)
+                target_var = target.to(self.device)
 
-            output = model(input_var)
+                output = model(input_var)
 
-            loss = self.criterion(output, target_var)
+                loss = self.criterion(output, target_var)
+                losses.update(loss.item())
+                metric_val = self.metric(output, target_var)
+                metrics.update(metric_val)
 
-            # measure accuracy and record loss
-            losses.update(loss.detach(), input.size(0))
+                self.watcher.display_every_iter(batch_idx, input_var, target, output)
 
-            metric_val = self.metric(output, target_var)
-            acc.update(metric_val)
-
-            self.watcher.log_value(VAL_ACC, metric_val)
-            self.watcher.log_value(VAL_LOSS, loss.detach())
-            self.watcher.display_every_iter(batch_idx, input_var, target, output)
-
-            # measure elapsed time
             batch_time.update(time.time() - end)
             end = time.time()
 
-            print('\rValidation: [{0}/{1}]\t'
-                  'ETA: {time:.0f}/{eta:.0f} s\t'
-                  'loss {loss.avg:.4f}\t'
-                  'metric {acc.avg:.4f}\t'.format(
-                batch_idx, len(val_loader), eta=batch_time.avg * len(val_loader),
-                time=batch_time.sum, loss=losses, acc=acc), end='')
+        print('\rValidation: [{0}/{1}]\t'
+              'ETA: {time:.0f}/{eta:.0f} s\t'
+              'loss {loss.avg:.4f}\t'
+              'metric {acc.avg:.4f}\t'.format(
+            batch_idx, len(val_loader), eta=batch_time.avg * len(val_loader),
+            time=batch_time.sum, loss=losses, acc=metrics), end='')
+
+        self.update_val_epoch_stat(losses.avg, metrics.avg)
+
+        self.watcher.log_value(VAL_ACC, metrics.avg)
+        self.watcher.log_value(VAL_LOSS, losses.avg)
         print()
         self.scheduler.step(losses.avg)
 
-        if self.is_best(losses.avg) and self.epoch_num % 3 == 0:
+        if self.is_best(losses.avg):
             self.save_checkpoint(model.state_dict(), self.get_checkpoint_name(losses.avg))
+            # pickle.dump(model, open(self.get_checkpoint_name(losses.avg), 'wb'))
 
         self.epoch_num += 1
-        return losses.avg, acc.avg
+        return losses.avg, metrics.avg
+
+    def update_train_epoch_stats(self, loss, metric):
+        self.epoch_train_losses.append(loss)
+        self.epoch_train_metrics.append(metric)
 
     def train(self, train_loader, model, epoch):
         batch_time = AverageMeter()
@@ -112,37 +130,39 @@ class Trainer(object):
 
         end = time.time()
         for batch_idx, (input, target) in enumerate(train_loader):
-
             data_time.update(time.time() - end)
 
-            input_var = torch.autograd.Variable(input.to(self.device))
-            target_var = torch.autograd.Variable(target.to(self.device))
+            input_var = input.to(self.device)
+            target_var = target.to(self.device)
 
-            self.optimizer.zero_grad()
             output = model(input_var)
+
             loss = self.criterion(output, target_var)
 
+            self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
 
-            losses.update(loss.detach(), input.size(0))
-
-            metric_val = self.metric(output, target_var)  # todo - add output dimention assertion
-            acc.update(metric_val, batch_idx)
-
-            self.watcher.log_value(TRAIN_ACC_OUT, metric_val)
-            self.watcher.log_value(TRAIN_LOSS_OUT, loss.detach())
-            self.watcher.display_every_iter(batch_idx, input_var, target, output)
+            with torch.no_grad():
+                losses.update(loss.item())
+                metric_val = self.metric(output, target_var)  # todo - add output dimention assertion
+                acc.update(metric_val)
+                self.watcher.display_every_iter(batch_idx, input_var, target, output)
 
             # measure elapsed time
             batch_time.update(time.time() - end)
             end = time.time()
 
-            print('\rEpoch: {0}  [{1}/{2}]\t'
-                  'ETA: {time:.0f}/{eta:.0f} s\t'
-                  'data loading: {data_time.val:.3f} s\t'
-                  'loss {loss.avg:.4f}\t'
-                  'metric {acc.avg:.4f}\t'.format(
-                epoch, batch_idx, len(train_loader), eta=batch_time.avg * len(train_loader),
-                time=batch_time.sum, data_time=data_time, loss=losses, acc=acc), end='')
+        logger.debug('\rEpoch: {0}  [{1}/{2}]\t'
+                     'ETA: {time:.0f}/{eta:.0f} s\t'
+                     'data loading: {data_time.val:.3f} s\t'
+                     'loss {loss.avg:.4f}\t'
+                     'metric {acc.avg:.4f}\t'.format(
+            epoch, batch_idx, len(train_loader), eta=batch_time.avg * len(train_loader),
+            time=batch_time.sum, data_time=data_time, loss=losses, acc=acc))
+
+        self.update_train_epoch_stats(losses.avg, acc.avg)
+
+        self.watcher.log_value(TRAIN_ACC_OUT, metric_val)
+        self.watcher.log_value(TRAIN_LOSS_OUT, loss.item())
         return losses.avg, acc.avg
